b5d8f5e7e5fd4810b4f37cc32946fe60f2fd4fbf
Naveen Deva <naveendeva909@gmail.com>
2024-09-19 10:46:33 +0530

    debug the userRoute file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUserToRoom } from "../controllers/userController";
+import { addUserToRoom } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
